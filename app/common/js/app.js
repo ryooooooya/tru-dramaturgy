0846e1ab23dc975b1808605484219ef2e7fbdffe
Chihara <chihara.ryoya@gmail.com>
2021-03-31 18:55:36 +0900

    upload [revert]

--- app/common/js/app.js
+++ app/common/js/app.js
@@ -26,16 +26,16 @@ $(function() {
 
 
 /* scroll */
-$(function(){
-  $('a[href^="#"]').click(function(){
-    var speed = 300;
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $("html, body").animate({scrollTop:position-68}, speed, "swing");
-    return false;
-  });
-});
+// $(function(){
+//   $('a[href^="#"]').click(function(){
+//     var speed = 300;
+//     var href= $(this).attr("href");
+//     var target = $(href == "#" || href == "" ? 'html' : href);
+//     var position = target.offset().top;
+//     $("html, body").animate({scrollTop:position-68}, speed, "swing");
+//     return false;
+//   });
+// });
 
 
 /* accordion */
@@ -66,3 +66,33 @@ $(function(){
     }
   });
 });
+
+// bg
+//付与したいクラスの配列
+$(function(){
+  var arr = ["bg01", "bg02", "bg03", "bg04"];
+  var a = arr.length;
+  //シャッフルアルゴリズム
+  while (a) {
+    var j = Math.floor(Math.random() * a);
+    var t = arr[--a];
+    arr[a] = arr[j];
+    arr[j] = t;
+  }
+  $("#article").addClass(arr[0]);
+});
+
+// アンカー
+$(window).on('load',function(){
+	$(function() {
+    let url = location.href;
+    if(url.indexOf("?id=") != -1){
+      let id = url.split("?id=");
+      let $target = $('#' + id[id.length - 1]);
+      if($target.length){
+        let position = $target.offset().top;
+        $("html, body").animate({scrollTop:position}, 1);
+      }
+    }
+	});
+})
